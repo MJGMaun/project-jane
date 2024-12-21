6068be93df07f81935115833ef4e53cfb7a36ba8
Mikha <mjmaun.dev@gmail.com>
2024-12-22 00:37:21 +0800

    setup hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from './components/Hero.jsx'
 
 const App = () => {
   return (
-	<h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+	<main className="relative min-h-screen w-screen overflow-x-hidden">
+		<Hero />
+		<section className="z-0 min-h-screen bg-blue-500"></section>
+	</main>
   )
 }
 
